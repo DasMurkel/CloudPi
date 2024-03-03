c1430daf2428eab05a38ac7761782ac6bb1c2e33
Matze <dasmurkel@gmail.com>
2024-03-03 17:16:31 +0100

    feat: Downgrade to OMV 6

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,33 +2,33 @@
 
 if [ "$1" = "installOMV" ]
 then
-    # as taken from https://forum.openmediavault.org/index.php?thread/50222-install-omv7-on-debian-12-bookworm/
-    cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
-deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm main
+    # as taken from https://forum.openmediavault.org/index.php?thread/39490-install-omv6-on-debian-11-bullseye/
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb http://packages.openmediavault.org/public shaitan main
+# deb http://downloads.sourceforge.net/project/openmediavault/packages shaitan main
 ## Uncomment the following line to add software from the proposed repository.
-#deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm-proposed main
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm-proposed main
+# deb http://packages.openmediavault.org/public shaitan-proposed main
+# deb http://downloads.sourceforge.net/project/openmediavault/packages shaitan-proposed main
 ## This software is not part of OpenMediaVault, but is offered by third-party
 ## developers as a service to OpenMediaVault users.
-#deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm partner
-#deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm partner
+# deb http://packages.openmediavault.org/public shaitan partner
+# deb http://downloads.sourceforge.net/project/openmediavault/packages shaitan partner
 EOF
 
-    export LANG=C.UTF-8
-    export DEBIAN_FRONTEND=noninteractive
-    export APT_LISTCHANGES_FRONTEND=none
-    apt-get install --yes gnupg
-    wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
-    apt-get update
-    apt-get --yes --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault
+export LANG=C.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+apt-get install --yes gnupg
+wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | gpg --dearmor | tee "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg"
+apt-get update
+apt-get --yes --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault
 
-    # Populate the database.
-    omv-confdbadm populate
+# Populate the database.
+omv-confdbadm populate
 
-    # Display the login information.
-    omv-salt deploy run hosts
-    cat /etc/issue
+# Display the login information.
+omv-salt deploy run hosts
+cat /etc/issue
 
 elif [ "$1" = "postOMV" ]
 then
