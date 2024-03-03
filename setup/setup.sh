e171a821f518357613ed6232606ca013ec857560
Matze <dasmurkel@gmail.com>
2024-03-03 14:41:03 +0100

    feat: Update setup.sh to install OMV 7

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,8 +2,34 @@
 
 if [ "$1" = "installOMV" ]
 then
-    wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install
-    bash install
+    # as taken from https://forum.openmediavault.org/index.php?thread/50222-install-omv7-on-debian-12-bookworm/
+    cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm main
+## Uncomment the following line to add software from the proposed repository.
+#deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm-proposed main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm-proposed main
+## This software is not part of OpenMediaVault, but is offered by third-party
+## developers as a service to OpenMediaVault users.
+#deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm partner
+#deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm partner
+EOF
+
+    export LANG=C.UTF-8
+    export DEBIAN_FRONTEND=noninteractive
+    export APT_LISTCHANGES_FRONTEND=none
+    apt-get install --yes gnupg
+    wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
+    apt-get update
+    apt-get --yes --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault
+
+    # Populate the database.
+    omv-confdbadm populate
+
+    # Display the login information.
+    omv-salt deploy run hosts
+    cat /etc/issue
+
 elif [ "$1" = "postOMV" ]
 then
     docker || echo "Docker not yet installed. Please install docker" ; exit 1
